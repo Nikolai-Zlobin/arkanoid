92d8e6aaf50736f982ffd23559fe40380235c451
NikolayZlobin <nikolai_zlobin@protonmail.com>
2020-07-20 21:35:31 +0700

    Added,  platform move

--- game.js
+++ game.js
@@ -1,16 +1,40 @@
+const keys = {
+    left: 37,
+    right: 39
+};
+
 let game = {
     ctx: null,
     platform: null,
     ball: null,
+    blocks: [],
+    rows: 4,
+    cols: 8,
+
     sprites: {
         background: null,
         ball: null,
-        platform: null
+        platform: null,
+        block: null,
     },
 
     init: function () {
         // Инициализация нашей игры
         this.ctx = document.getElementById('mycanvas').getContext('2d');
+        this.setEvents();
+    },
+    // Двигаем платформу
+    setEvents() {
+        window.addEventListener('keydown', e => {
+            if (e.keyCode === keys.left || e.keyCode === keys.right) {
+                this.platform.start(e.keyCode);
+            }
+        });
+
+        window.addEventListener('keyup', e => {
+            this.platform.stop();
+            this.platform.dx = 0;
+        });
     },
 
     preload: function (callback) {
@@ -18,26 +42,40 @@ let game = {
         let loaded = 0;
         let required = Object.keys(this.sprites).length; //Количество спрайтов
 
+        let onImageLoad = () => {
+            ++loaded;
+            if (loaded >= required) {
+                callback();
+            }
+        }
         //Цикл добавляющий к каждому спрайту картинку, а так же контроль загрузки картинок
         for (let key in this.sprites) {
             this.sprites[key] = new Image();
             this.sprites[key].src = `img/${key}.png`;
-
-            this.sprites[key].addEventListener('load', () => {
-                ++loaded;
-
-                if (loaded >= required) {
-                    callback();
-                }
-            });
-
+            this.sprites[key].addEventListener('load', onImageLoad);
         }
+    },
+    create: function () {
+        for (let row = 0; row < this.rows; row++) {
+            for (let col = 0; col < this.cols; col++) {
+                this.blocks.push({
+                    x: 64 * col + 65,
+                    y: 24 * row + 35
+                });
+            }
+        }
+    },
+    // Изменения в игровом поле
+    update: function () {
+        this.platform.move();
     },
 
     run: function () {
         // Запуск игры
         window.requestAnimationFrame(() => {
+            this.update();
             this.render();
+            this.run();
         });
     },
 
@@ -46,12 +84,19 @@ let game = {
         this.ctx.drawImage(this.sprites.background, 0, 0);
         this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
         this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+        this.renderBlocks();
+    },
 
+    renderBlocks() {
+        for (let block of this.blocks) {
+            this.ctx.drawImage(this.sprites.block, block.x, block.y);
+        }
     },
 
     start: function () {
         this.init();
         this.preload(() => {
+            this.create();
             this.run();
         });
     }
@@ -66,8 +111,30 @@ game.ball = {
 }
 
 game.platform = {
+    velocity: 6,
+    dx: 0,
     x: 280,
-    y: 300
+    y: 300,
+
+    start(direction) {
+        if (direction === keys.left) {
+            this.dx = - this.velocity;
+        }
+        else if (direction === keys.right) {
+            this.dx = this.velocity;
+        }
+    },
+
+    stop() {
+        this.dx = 0;
+    },
+
+    move() {
+        if (this.dx) {
+            this.x += this.dx;
+            game.ball.x += this.dx;
+        }
+    }
 };
 
 // Объект window на который установлен обработчик событий
